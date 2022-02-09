cbdd12e73acc670ae7c6a5e50a134c16712c91b7
Nursat <nursatnb@mail.ru>
2022-02-09 21:57:13 +0500

    Changed icon, added NotFound page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { Details } from './pages/Details';
 import { HomePage } from './pages/HomePage';
-
+import { NotFound } from './pages/NotFound';
 
 const App = () => {
 
@@ -17,6 +17,7 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<HomePage countries={countries} setCountries={setCountries} />} />
           <Route path='/country/:name' element={<Details />} />
+          <Route element={<NotFound />} />
         </Routes>
       </Main>
     </BrowserRouter>
